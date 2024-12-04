73ab97a184ef67a1c3af4c437f73d6430de19885
acemods <acemods@me.com>
2024-12-04 08:11:30 +0000

    netlify fix again

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,18 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useGameStatus = rowsCleared => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(1);
 
-  const linePoints = [40, 100, 300, 1200];
+  const getLinePoints = useCallback(() => [40, 100, 300, 1200], []);
 
   useEffect(() => {
     if (rowsCleared > 0) {
-      setScore(prev => prev + linePoints[rowsCleared - 1] * level);
+      const points = getLinePoints()[rowsCleared - 1] * level;
+      setScore(prev => prev + points);
       setRows(prev => prev + rowsCleared);
     }
-  }, [level, rowsCleared, linePoints]);
+  }, [level, rowsCleared, getLinePoints]);
 
   return [score, setScore, rows, setRows, level, setLevel];
 }; 
